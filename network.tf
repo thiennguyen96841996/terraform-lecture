e698b32563dc5b56af671fb50065bd69fcbbf21a
--list <nguyen.h@tsunagu-grp.jp>
2023-11-30 16:28:48 +0900

    Create internet gateway

--- network.tf
+++ network.tf
@@ -122,3 +122,24 @@ resource "aws_route_table_association" "private_rt_1c" {
   route_table_id = aws_route_table.private_rt.id  // ルートテーブルID
   subnet_id      = aws_subnet.private_subnet_1c.id  // サブネットID
 }
+
+# ---------------------------------------------
+# Internet Gateway
+# ---------------------------------------------
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id // VPC ID
+
+  // タグ
+  tags = {
+    Name    = "${var.project}-${var.environment}-igw"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+// ルート
+resource "aws_route" "public_rt_igw_r" {
+  route_table_id         = aws_route_table.public_rt.id // ルートテーブルID
+  destination_cidr_block = "0.0.0.0/0"  // 送信先
+  gateway_id             = aws_internet_gateway.igw.id  // インターネットゲートウェイID
+}
