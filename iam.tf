a21215da78e749d2546678fbc6f4dcb7f34b033e
--list <nguyen.h@tsunagu-grp.jp>
2024-03-15 15:18:59 +0900

    ポリシーアタッチ

--- iam.tf
+++ iam.tf
@@ -16,3 +16,24 @@ data "aws_iam_policy_document" "ec2_assume_role" {
     }
   }
 }
+
+// ポリシーアタッチ
+resource "aws_iam_role_policy_attachment" "app_iam_role_ec2_readonly" {
+  role       = aws_iam_role.app_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"  // ポリシーARN・コンソールに確認する
+}
+
+resource "aws_iam_role_policy_attachment" "app_iam_role_ssm_managed" {
+  role       = aws_iam_role.app_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "app_iam_role_ssm_readonly" {
+  role       = aws_iam_role.app_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "app_iam_role_s3_readonly" {
+  role       = aws_iam_role.app_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
